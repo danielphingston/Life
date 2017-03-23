7eb483cd9b98c392315d0a843bc864f86fb2b1c0
Daniel Phingston <danielphingston@gmail.com>
2017-03-23 21:04:01 +0530

    Add files via upload

--- Request.php
+++ Request.php
@@ -9,15 +9,42 @@
 	$state = $_POST["state"];
 	$city = $_POST["city"];
 	$reason = $_POST["reason"];
+        $username=$_POST["username"];
    
 
-    $statement = mysqli_prepare($con, "INSERT INTO request ( organs, blood, contact, age, state, city, reason) VALUES (?,?,?,?,?,?,?)");
-    mysqli_stmt_bind_param($statement,"ssiisss",$organ,$blood,$contact,$age,$state,$city,$reason);
+    $statement = mysqli_prepare($con, "INSERT INTO request ( organ, blood, contact, age, state, city, reason,username) VALUES (?,?,?,?,?,?,?,?)");
+    mysqli_stmt_bind_param($statement,"ssiissss",$organ,$blood,$contact,$age,$state,$city,$reason,$username);
     mysqli_stmt_execute($statement);
-    
+
+function usernameAvailable() {
+        global $con, $username;
+        $statement = mysqli_prepare($con,  "SELECT * FROM request WHERE username = ?"); 
+        mysqli_stmt_bind_param($statement, "s", $username);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_store_result($statement);
+        $count = mysqli_stmt_num_rows($statement);
+        mysqli_stmt_close($statement); 
+        if ($count < 1){
+            return true; 
+        }else {
+            return false; 
+        }
+    }  
+  
+
     $response = array();
-    $response["success"] = true;  
+   $response["success"] = false;      
+
     
-    print_r(json_encode($response));
+
+    if (usernameAvailable()){
+               $response["success"] = true;  
+    }
+    else{ 
+     $response["success"] = false; 
+     $response["exist"]=true;  }
+    
+ 
+   print_r(json_encode($response));
 
 ?>
